c2eb4a891e381c2277269b192c82216436ca6d17
esnhamden <236940815+esnhamden@users.noreply.github.com>
2025-12-08 20:52:11 -0800

    Update react-server.cjs

--- frontend/react-server.cjs
+++ frontend/react-server.cjs
@@ -1,3 +1,12 @@
+/*
+Alexander Birrell and Eason Hamden
+12/8/2025
+
+Citations:
+- Copied from CS340 Activity 2 Connect webapp to database
+  URL: https://canvas.oregonstate.edu/courses/2017561/assignments/10111722?module_item_id=25645065
+*/
+
 const express = require("express");
 const path = require("path");
 
